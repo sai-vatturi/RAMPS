792cc62cbbf0956365544e7313edbcfa8bfac677
Sainadh Vatturi <sainadhvatturi@gmail.com>
2024-12-08 11:31:07 +0530

    update angular.json

---
+++ frontend/postcss.config.js
@@ -0,0 +1,8 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+	content: ['./src/**/*.html', './src/**/*.ts'],
+	defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
+module.exports = {
+	plugins: [require('autoprefixer'), ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])]
+};

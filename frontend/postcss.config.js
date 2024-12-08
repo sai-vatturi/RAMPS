f217003baf6e3f2968e5ef174a497c42300155c9
Sainadh Vatturi <sainadhvatturi@gmail.com>
2024-12-08 12:19:25 +0530

    update frontend

--- frontend/postcss.config.js
+++
@@ -1,8 +0,0 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-	content: ['./src/**/*.html', './src/**/*.ts'],
-	defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-});
-
-module.exports = {
-	plugins: [require('autoprefixer'), ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])]
-};

f05d845393d5cdd817bce79562dbf5cb6173a1ea
Sainadh V <115538210+sai-vatturi@users.noreply.github.com>
2025-01-19 20:58:15 +0530

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-	production: false,
-	// apiUrl: 'http://15.207.100.163:8080/'
-	apiUrl: 'http://localhost:5228/'
+	production: true,
+	apiUrl: 'https://recipemeal-a2bbege7ffhabbgn.australiaeast-01.azurewebsites.net/'
+	// apiUrl: 'http://localhost:5228/'
 };

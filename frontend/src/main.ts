7c23b55a32be728d412fc9b61edf7ed0784e2723
sai-vatturi <sainadhvatturi@gmail.com>
2025-01-20 08:09:10 +0530

    use hash location strategy

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,3 +1,4 @@
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
@@ -8,9 +9,5 @@ import { routes } from './app/app.routes';
 import { authInterceptor } from './app/interceptors/auth.interceptor';
 
 bootstrapApplication(AppComponent, {
-	providers: [
-		provideRouter(routes),
-		provideHttpClient(withInterceptors([authInterceptor])), // Use functional interceptor
-		importProvidersFrom(BrowserAnimationsModule)
-	]
+	providers: [provideRouter(routes), provideHttpClient(withInterceptors([authInterceptor])), importProvidersFrom(BrowserAnimationsModule), { provide: LocationStrategy, useClass: HashLocationStrategy }]
 }).catch(err => console.error(err));

160be73a0811fbfbddea7acd3d0c31c57d906f3c
Sainadh V <sainadhvatturi@gmail.com>
2025-02-05 20:29:25 +0530

    update env

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
 	production: true,
-	apiUrl: 'https://foodpro-cybffwccawbzd5ej.canadacentral-01.azurewebsites.net/'
+	apiUrl: 'https://foodpro.whiteplant-3aefddd0.westus2.azurecontainerapps.io/'
 	// apiUrl: 'http://localhost:5228/'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
 	production: false,
-	apiUrl: 'https://foodpro-cybffwccawbzd5ej.canadacentral-01.azurewebsites.net/'
+	apiUrl: 'https://foodpro.whiteplant-3aefddd0.westus2.azurecontainerapps.io/'
+
 	// apiUrl: 'http://localhost:5228/'
 };
